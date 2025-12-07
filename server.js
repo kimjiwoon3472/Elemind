6b592b72371c92a3ccec135166d49929c64e364a
kimjiwoon3472 <25_17@bssm.hs.kr>
2025-12-07 23:54:29 +0900

    fix DB env variables

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const mysql = require('mysql2/promise');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // MySQL 연결 풀 생성
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'ptuser',
-  password: 'ptpassword',
-  database: 'periodic_table',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
